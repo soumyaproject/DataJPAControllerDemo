e768a027dbbe4cbd901306247e3b51a60a97c79e
soumyaproject <soumyaranjanpanda95935@gmail.com>
2025-06-18 10:20:08 +0530

    new changes in new branches

--- src/test/java/com/example/demo/DataJpaRepositoryDemoApplicationTests.java
+++ src/test/java/com/example/demo/DataJpaRepositoryDemoApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.demo;
 
+import com.example.demo.Controller.Controllerdemo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class DataJpaRepositoryDemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	@Test
+	public void testRealme() {
+		Controllerdemo controller = new Controllerdemo();
+		String result = controller.realme();
+		assertEquals("hey-This method is only for testing", result);
 	}
 
 }
